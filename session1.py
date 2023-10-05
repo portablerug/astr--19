efedfaa069c2a98ab30bd58cbe34c9a597b2f69c
Miguel Juarez <115324080+yakuzaza@users.noreply.github.com>
2023-10-04 20:09:49 -0700

    Added session1.py
    This Python code tells the user what my name is, my preferred pronouns, and what my favorite movie and food is. I used simple name variables to store the answers and them to be plugged into the string once run.

---
+++ session1.py
@@ -0,0 +1,13 @@
+
+
+name = "Miguel Juarez"
+pronouns = "he/him"
+movie = "Fantastic Mr.Fox"
+food = "Cheeseburger"
+
+print("Hello, my name is", name)
+print("My pronouns are", pronouns)
+print("My favorite movie is", movie, "because I enjoy the stop animation and think Mr.Fox is a funny charachter.")
+print("My favorite food are", food, "just because they are everywhere and I can have a food I like at almost any place I go out to eat.")
+
+
