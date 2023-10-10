ab2aaae1d4a1e0210c2e73fa70d29f642136530d
Miguel Juarez <115324080+yakuzaza@users.noreply.github.com>
2023-10-09 18:45:59 -0700

    Added session2.py program file
    This program runs a defined x and y value of an integer or floating point number. The output prints out what the predefined x and y variables were defined as, while printing the sum/difference/product of the x and y values. The output also prints out the data type of each answer.

---
+++ session2.py
@@ -0,0 +1,18 @@
+x = 15.04
+y = 23.04
+print(x, y)
+print("The sum of these two floating numbers is",x+y, type(x+y))
+
+x = 15
+y = 23 
+
+print(x, y)
+print("The difference between these integers is ",x-y, type(x-y))
+
+x = 31.01
+y = 19
+
+print(x, y)
+print("The product of this flaoting point number and integer is",x*y, type(x*y))
+
+
